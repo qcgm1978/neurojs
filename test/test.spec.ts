46188081e6d5ff1eb94a4062fc1b011ad102d9fe
zhanghl-k <zhanghl-k@glodon.com>
2018-04-09 10:57:05 +0800

    p2

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,7 +1,8 @@
 import * as assert from 'assert';
 import { expect } from 'chai';
 const window = {}
-import { boot } from '../examples/cars/src/index'
+// import { boot } from '../examples/cars/src/index'
+import * as p2 from 'p2'
 describe('Array', function () {
     describe('#indexOf()', function () {
         it('should return -1 when the value is not present', function () {
@@ -12,10 +13,13 @@ describe('Array', function () {
         expect(1).to.be.within(0, Infinity);
     });
 });
-describe(`cars`, () => {
-    describe(`gcd`, () => {
-        it(`gcd`, () => {
-            expect(boot).to.be.an()
+describe(`https://github.com/schteppe/p2.js`, () => {
+    describe(`The following example uses the World, Circle, Body and Plane classes to set up a simple physics scene with a ball on a plane.`, () => {
+        it(`Create a physics world, where bodies and constraints live`, () => {
+            const world = new p2.World({
+                gravity: [0, -9.82]
+            });
+            expect(world).to.be.an('object')
         });
     })
 });
